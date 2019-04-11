65b298089cb47f01621c5001075ad1b69ef9b689
小帅哥 <kaapoole@gmail.com>
2019-04-11 18:07:59 +0800

    添加详情页面

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,5 +1,5 @@
 // pages/posts/posts.js
-// z这里只能用相对路径  不能用绝对路径
+// 引入脚本文件 这里只能用相对路径  不能用绝对路径
 var postData = require('../../data/posts-data.js')
 
 Page({
@@ -18,7 +18,14 @@ Page({
     this.setData({
       posts_key: postData.postList
     });
-    
+
+  },
+  jumpPotoDetail:function(even){
+    var posID = even.currentTarget.dataset.posid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail',
+    })
   }
+
 
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -3,9 +3,9 @@ Page({
   jumpPosts:function(){
     // 调用navigateTo 会保留当前页面  
     // 使用左上角返回能返回上级页面
-    wx.navigateTo({
-      url: '../posts/posts',
-    })
+    // wx.navigateTo({
+    //   url: '../posts/posts',
+    // })
     // 调用 redirectTo 会销毁当前页面  不会保留 所以无法
     // 返回前一个页面
     wx.redirectTo({
