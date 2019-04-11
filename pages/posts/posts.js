2d5f5e9bddb47a793a1ab55d1170a9f0c6829012
小帅哥 <kaapoole@gmail.com>
2019-04-11 14:12:00 +0800

    添加posts页面数据

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,14 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    date:'Niv 19 2019',
+    title:'正是虾肥蟹壮时'
   },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var post_content = {
+      date:"Nov 18 2019",
+      title:'正是虾肥蟹壮时',
+      img:{
+        author_img: '/images/avatar/1.png',
+        post_img: '/images/post/crab.png'
+      },
+      content:'用于指定小程序由哪些页面组成，每一项都对应一个页面的 路径（含文件名） 信息。文件名不需要写文件后缀，框架会自动去寻找对于位置的 .json, .js, .wxml, .wxss 四个文件进行处理。',
+      view_num:'112',
+      collect_num:'96',
+    }
+    this.setData(post_content);
   },
 
   /**
