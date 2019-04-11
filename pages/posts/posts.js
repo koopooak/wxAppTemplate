cb645085d581a2611a73aa3f65ef947d170fdb2e
小帅哥 <kaapoole@gmail.com>
2019-04-11 15:36:37 +0800

    添加数据

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,75 +5,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-    date:'Niv 19 2019',
-    title:'正是虾肥蟹壮时'
+    date: 'Niv 19 2019',
+    title: '正是虾肥蟹壮时',
+    posts_key: []
   },
 
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var post_content = {
-      date:"Nov 18 2019",
-      title:'正是虾肥蟹壮时',
-      img:{
-        author_img: '/images/avatar/1.png',
-        post_img: '/images/post/crab.png'
+  onLoad: function(options) {
+    var post_content = [{
+        date: "Nov 18 2019",
+        title: '正是虾肥蟹壮时',
+        post_img: '/images/post/crab.png',
+        content: '用于指定小程序由哪些页面组成，每一项都对应一个页面的 路径（含文件名） 信息。文件名不需要写文件后缀，框架会自动去寻找对于位置的 .json, .js, .wxml, .wxss 四个文件进行处理。',
+        view_num: '112',
+        collect_num: '96',
+        author_img: '/images/avatar/1.png'
       },
-      content:'用于指定小程序由哪些页面组成，每一项都对应一个页面的 路径（含文件名） 信息。文件名不需要写文件后缀，框架会自动去寻找对于位置的 .json, .js, .wxml, .wxss 四个文件进行处理。',
-      view_num:'112',
-      collect_num:'96',
-    }
-    this.setData(post_content);
+      {
+        date: "Nov 1 2029",
+        title: '比利·林恩的中场故事',
+        post_img: '/images/post/bl.png',
+        content: '如果小程序是一个多 tab 应用（客户端窗口的底部或顶部有 tab 栏可以切换页面），可以通过 tabBar 配置项指定 tab 栏的表现，以及 tab 切换时显示的对应页面。',
+        view_num: '112',
+        collect_num: '96',
+        author_img: '/images/avatar/2.png'
+      }
+    ]
+    this.setData({
+      posts_key: post_content
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,18 @@
 Page({
-  
+  // 跳转 阅读页面
+  jumpPosts:function(){
+    // 调用navigateTo 会保留当前页面  
+    // 使用左上角返回能返回上级页面
+    wx.navigateTo({
+      url: '../posts/posts',
+    })
+    // 调用 redirectTo 会销毁当前页面  不会保留 所以无法
+    // 返回前一个页面
+    wx.redirectTo({
+      url: '../posts/posts',
+    })
+  }
+
+
+
 })
